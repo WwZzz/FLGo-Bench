014b7067e616ef0b19ff531c19883214fe92d628
WwZzz <zwang@stu.xmu.edu.cn>
2023-09-14 00:08:16 +0800

    update

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 ############################Task cifar10_dir1_20
 ######################## P=1.0
-# fedavg
-python run.py --task cifar10_dir1_c20 --method fedavg --config ./EXP_R1000_P1._V.2/config_fedavg.yml --tune --gpu 0 1 2 3 4 5 6 7  > fedavg_r1000_p1_v2.txt
-python run.py --task cifar10_dir1_c20 --method fedavg --config ./EXP_R1000_P1._V.2/op_config_fedavg_cifar10_dir1_c20.yml --gpu 0 1 2 3 4 > fedavg_r1000_p1_v2.txt
-# fedprox
-python run.py --task cifar10_dir1_c20 --method fedprox --config ./EXP_R1000_P1._V.2/config_fedprox.yml --tune --gpu 3 4 5 6 7 > fedprox_r1000_p1_v2.txt
-python run.py --task cifar10_dir1_c20 --method fedprox --config ./EXP_R1000_P1._V.2/op_config_fedprox_cifar10_dir1_c20.yml --gpu 4 5 6 7 > fedprox_r1000_p1_v2.txt
+## fedavg
+#python run.py --task cifar100_dir1_c20 --method fedavg --config ./EXP_R1000_P1._V.2/config_fedavg.yml --tune --gpu 0 2 3 4 5  > fedavg_r1000_p1_v2.txt
+#python run.py --task cifar100_dir1_c20 --method fedavg --config ./EXP_R1000_P1._V.2/op_config_fedavg_cifar100_dir1_c20.yml --gpu 0 2 3 4 5
+
+
 
 ####################### P=0.1
+# fedavg
+python run.py --task cifar100_dir1_c20 --method fedavg --config ./EXP_R1000_P.1_V.2/config_fedavg.yml --tune --gpu 0 1 2 3 4 5  > fedavg_r1000_p1_v2.txt
+python run.py --task cifar100_dir1_c20 --method fedavg --config ./EXP_R1000_P.1._V.2/op_config_fedavg_cifar100_dir1_c20.yml --gpu 0 1 2 3 4 5
