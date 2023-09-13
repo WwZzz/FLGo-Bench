5cf1fa2da20e65641cec8a352d30350c6d33d2c9
WwZzz <zwang@stu.xmu.edu.cn>
2023-09-14 00:21:04 +0800

    ADD new para

--- run.sh
+++ run.sh
@@ -4,7 +4,10 @@
 #python run.py --task cifar100_dir1_c20 --method fedavg --config ./EXP_R1000_P1._V.2/config_fedavg.yml --tune --gpu 0 2 3 4 5  > fedavg_r1000_p1_v2.txt
 #python run.py --task cifar100_dir1_c20 --method fedavg --config ./EXP_R1000_P1._V.2/op_config_fedavg_cifar100_dir1_c20.yml --gpu 0 2 3 4 5
 
-
+####################### P=0.2
+# fedavg
+python run.py --task cifar100_dir1_c20 --method fedavg --config ./EXP_R1000_P.2_V.2/config_fedavg.yml --tune --gpu 0 1 2 3 4 5  > fedavg_r1000_p1_v2.txt
+python run.py --task cifar100_dir1_c20 --method fedavg --config ./EXP_R1000_P.2._V.2/op_config_fedavg_cifar100_dir1_c20.yml --gpu 0 1 2 3 4 5
 
 ####################### P=0.1
 # fedavg
