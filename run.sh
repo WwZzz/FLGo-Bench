e5e898c53179e6295fc83b48da1e99c40ddd5168
zzz <zzz510711928@gmail.com>
2024-07-20 23:04:49 +0800

    add runing cmd for prox on mnist and cifar

--- run.sh
+++ run.sh
@@ -13,3 +13,8 @@ nohup python run.py --task mnist_dir5.0_c100 --algorithm fedavg --gpu 3 0 1 2 --
 nohup python run.py --task agnews_dir1.0_c100 --algorithm fedavg --gpu 1 2 3 0 --config ./config/tmp.yml --max_pdev 1 --put_interval 30 --available_interval 30 &
 
 nohup python run.py --task cifar10_iid_c100 --algorithm fedprox --gpu 1 2 3 0 --config ./config/run_prox.yml &
+nohup python run.py --task mnist_iid_c100 --algorithm fedprox --gpu 0 1 2 3 --config ./config/prox_iid.yml &
+nohup python run.py --task mnist_dir0.1_c100 --algorithm fedprox --gpu 0 1 2 3 --config ./config/prox_dir01.yml &
+nohup python run.py --task mnist_dir1.0_c100 --algorithm fedprox --gpu 1 2 3 0 --config ./config/prox_dir10.yml &
+nohup python run.py --task mnist_dir2.0_c100 --algorithm fedprox --gpu 2 3 0 1 --config ./config/prox_dir20.yml &
+nohup python run.py --task mnist_dir5.0_c100 --algorithm fedprox --gpu 3 0 1 2 --config ./config/prox_dir50.yml &
