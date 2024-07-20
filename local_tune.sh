9a8b50efcee4fbd842a283b908beeacb53a3c753
zzz <zzz510711928@gmail.com>
2024-07-21 01:18:16 +0800

    add tune for scaffold

--- local_tune.sh
+++ local_tune.sh
@@ -19,3 +19,8 @@ nohup python tune.py --task cifar10_dir1.0_c100 --gpu 3 1 2 0 --method fedprox -
 nohup python tune.py --task agnews_dir1.0_c100 --gpu 0 1 2 3 --method fedprox --config ./config/tune_prox.yml --put_interval 30 --available_interval 30 --max_pdev 1 &
 
 
+nohup python tune.py --task mnist_iid_c100 --gpu 0 1 2 3 --method scaffold &
+nohup python tune.py --task mnist_dir5.0_c100 --gpu 1 2 3 0 --method scaffold &
+nohup python tune.py --task mnist_dir2.0_c100 --gpu 2 3 0 1 --method scaffold &
+nohup python tune.py --task mnist_dir1.0_c100 --gpu 3 0 1 2 --method scaffold &
+nohup python tune.py --task mnist_dir0.1_c100 --gpu 0 1 2 3 --method scaffold &
