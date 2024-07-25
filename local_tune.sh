7d5a4f2249d0d89fc8120f19391ddb0088929098
zzz <zzz510711928@gmail.com>
2024-07-25 12:47:19 +0800

    add tuning office for fedavg

--- local_tune.sh
+++ local_tune.sh
@@ -12,6 +12,7 @@ nohup python tune.py --task mnist_dir0.1_c100 --gpu 0 1 2 3 --method fedavg
 nohup python tune.py --task agnews_dir1.0_c100 --gpu 0 1 2 3 --method fedavg --max_pdev 1 &
 
 nohup python tune.py --task domainnet_c6 --gpu 0 1 2 3 --method fedavg &
+nohup python tune.py --task office_caltech10_c4 --gpu 0 1 2 3 --method fedavg &
 
 nohup python tune.py --task mnist_iid_c100 --gpu 0 1 2 3 --method fedprox --config ./config/tune_prox.yml --put_interval 10 --available_interval 10 --max_pdev 10 &
 nohup python tune.py --task mnist_dir0.1_c100 --gpu 0 1 2 3 --method fedprox --config ./config/tune_prox.yml --put_interval 10 --available_interval 10 --max_pdev 10 &
