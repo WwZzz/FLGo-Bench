29d0ca93cc2c772f9a8337dfd684a842c48cbfb8
zzz <zzz510711928@gmail.com>
2024-08-23 13:06:18 +0800

    modify test example

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
+import flgo
 import flgo.algorithm.fedavg as fedavg
 import flgo.benchmark.mnist_classification as mnist
 import flgo.benchmark.partition as fbp
-import flgo.experiment.analyzer
 
 task = './my_task'
 flgo.gen_task_by_(mnist, fbp.IIDPartitioner(num_clients=100), task)
-flgo.init(task, fedavg, {'gpu':0, 'num_rounds':3}).run()
+flgo.init(task, fedavg, {'gpu':6, 'num_rounds':3}).run()
