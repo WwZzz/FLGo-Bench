70b7cf615ef0256df07ba8302684fb3ebf2f10b2
zzz <zzz510711928@gmail.com>
2024-09-26 23:54:42 +0800

    update get tune

--- get_tune_res.py
+++ get_tune_res.py
@@ -6,35 +6,13 @@ import flgo.experiment.analyzer as fea
 
 def read_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--task', help='name of task', type=str, default='')
-    parser.add_argument('--algorithm', help='name of method', type=str, default='fedavg')
+    parser.add_argument('--task', help='name of task', nargs='*', type=str, default=[])
+    parser.add_argument('--algorithm', help='name of method', nargs='*', type=str, default=['fedavg'])
     parser.add_argument('--model', help='name of model', type=str, default='')
     parser.add_argument('--config', type=str, help='configuration of hypara', default='')
     parser.add_argument('--metric', type=str, help='the name of metric', default='accuracy')
+    parser.add_argument('--simulator', help='name of simulator', type=str, default='')
     return parser.parse_known_args()
-
-args = read_args()[0]
-task =args.task
-
-if args.config!='' and os.path.exists(args.config):
-    with open(args.config, 'r') as inf:
-        option = yaml.load(inf, Loader=yaml.FullLoader)
-else: option = {}
-algorithm = args.algorithm
-config = args.config
-if args.model!='': option['model'] = [args.model]
-records = fea.load_records(os.path.join('task', task), algorithm, option)
-print(f"Number of Records: {len(records)}")
-painter = fea.Painter(records)
-try:
-    painter.create_figure(fea.Curve, {'args':{'x':'communication_round', 'y':'val_accuracy'}})
-except:
-    pass
-try:
-    painter.create_figure(fea.Curve, {'args':{'x':'communication_round', 'y':'local_val_accuracy'}})
-except:
-    pass
-tb = fea.Table(records)
 
 def get_column(tb, name):
     idx = tb.tb.field_names.index(name)
@@ -64,41 +42,39 @@ def optimal_round_by_local_val(x, op={'metric': 'accuracy'}):
     res = x.log.get('local_val_'+op['metric'], None)
     return np.argmax(res) if res is not None else -np.inf
 
-tb.add_column(max_local_val, {'metric':args.metric})
-tb.add_column(max_global_val, {'metric':args.metric})
-tb.add_column(lr)
-tb.add_column(col_model)
-tb.add_column(optimal_round_by_val, {'metric':args.metric})
-tb.add_column(optimal_round_by_local_val, {'metric':args.metric})
-sort_key =  max_local_val.__name__+'-'+args.metric
-gv = get_column(tb, max_global_val.__name__+'-'+args.metric)
-if len(gv)>0 and gv[0] is not None and gv[0]!=-np.inf:
-    sort_key = max_global_val.__name__+'-'+args.metric
-
-tb.tb.sortby = sort_key
-tb.print()
-# selector = fea.Selector({'task': task, 'header':[method], 'filter':config2filter(option)})
-# records = selector.records[task]
-# records = [r for r in records if 'Tune' not in r.name]
-# selector.all_records = records
-# grouped_records, groups = selector.group_records(key=['seed', 'gpu'])
-# painter = fea.Painter(grouped_records)
-# # create figs
-# fig1 = {'args':{'x':'communication_round', 'y':'test_accuracy'}, 'fig_option':{'xlabel':'communication_round', 'ylabel':'test_accuracy','title': "{}_on_{}".format(method, task)}}
-# fig2 = {'args':{'x':'communication_round', 'y':'test_loss'}, 'fig_option':{'xlabel':'communication_round', 'ylabel':'test_loss','title': "{}_on_{}".format(method, task)}}
-# fig3 = {'args':{'x':'communication_round', 'y':'mean_local_test_accuracy'}, 'fig_option':{'xlabel':'communication_round', 'ylabel':'mean_local_test_accuracy','title': "{}_on_{}".format(method, task)}}
-# fig4 = {'args':{'x':'communication_round', 'y':'mean_local_test_loss'}, 'fig_option':{'xlabel':'communication_round', 'ylabel':'mean_local_test_loss','title': "{}_on_{}".format(method, task)}}
-# fig5 = {'args':{'x':'communication_round', 'y':'std_local_test_accuracy'}, 'fig_option':{'xlabel':'communication_round', 'ylabel':'std_local_test_accuracy','title': "{}_on_{}".format(method, task)}}
-# fig6 = {'args':{'x':'communication_round', 'y':'std_local_test_loss'}, 'fig_option':{'xlabel':'communication_round', 'ylabel':'std_local_test_loss','title': "{}_on_{}".format(method, task)}}
-# figs = [fig1, fig2, fig3, fig4, fig5, fig6]
-# for fig in figs:
-#     painter.create_figure('GroupCurve', fig)
-# tabulor = fea.Table(grouped_records)
-# tabulor.add_column('group_optimal_value', {'x':'test_accuracy', 'flag':'max', 'name':'Acc.'})
-# tabulor.add_column('group_optimal_value', {'x':'mean_local_test_accuracy', 'flag':'max', 'name':'Local Acc.'})
-# tabulor.add_column('group_optimal_value', {'x':'test_loss', 'flag':'min','name':'Loss'})
-# tabulor.add_column('group_optimal_value', {'x':'mean_local_test_loss', 'flag':'min', 'name':'Local Loss'})
-# tabulor.add_column('group_optimal_x_by_y', {'x':'std_local_test_accuracy', 'y':'test_accuracy', 'flag':'max', 'name':'Std. Local Acc.'})
-# tabulor.add_column('group_optimal_x_by_y', {'x':'std_local_test_loss', 'y':'test_accuracy', 'flag':'max', 'name':'Std. Local Loss'})
-# tabulor.add_column('group_optimal_x_by_y', {'x':'communication_round', 'y':'test_accuracy', 'flag':'max', 'name':'Optimal Round'})
-# tabulor.print()
+if __name__ == '__main__':
+    args = read_args()[0]
+    if args.config!='' and os.path.exists(args.config):
+        with open(args.config, 'r') as inf:
+            option = yaml.load(inf, Loader=yaml.FullLoader)
+    else: option = {}
+    config = args.config
+    if args.model!='': option['model'] = [args.model]
+    if args.simulator!='': option['simulator'] = [args.simulator]
+    for task in args.task:
+        for algorithm in args.algorithm:
+            records = fea.load_records(os.path.join('task', task), algorithm, option)
+            print(f"Number of Records: {len(records)}")
+            painter = fea.Painter(records)
+            try:
+                painter.create_figure(fea.Curve, {'args':{'x':'communication_round', 'y':f"val_{config['metric']}"}})
+            except:
+                pass
+            try:
+                painter.create_figure(fea.Curve, {'args':{'x':'communication_round', 'y':f"local_val_{config['metric']}"}})
+            except:
+                pass
+            tb = fea.Table(records)
+            tb.add_column(max_local_val, {'metric':args.metric})
+            tb.add_column(max_global_val, {'metric':args.metric})
+            tb.add_column(lr)
+            tb.add_column(col_model)
+            tb.add_column(optimal_round_by_val, {'metric':args.metric})
+            tb.add_column(optimal_round_by_local_val, {'metric':args.metric})
+            sort_key =  max_local_val.__name__+'-'+args.metric
+            gv = get_column(tb, max_global_val.__name__+'-'+args.metric)
+            if len(gv)>0 and gv[0] is not None and gv[0]!=-np.inf:
+                sort_key = max_global_val.__name__+'-'+args.metric
+            tb.tb.sortby = sort_key
+            tb.tb.title = f"{task}-{algorithm}"
+            tb.print()
